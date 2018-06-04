142e9cdef6502a150846852ee2d28da9b13a835a
Ocean <dvsocean@icloud.com>
2018-06-04 11:10:47 -0700

    rebuild of service successful

---
+++ src/test/java/hello/BaseClass.java
@@ -0,0 +1,32 @@
+package hello;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ContractRestServiceApplication.class)
+public abstract class BaseClass {
+
+  @Autowired
+  PersonRestController personRestController;
+
+  @MockBean
+  PersonService personService;
+
+  @Before public void setup() {
+    RestAssuredMockMvc.standaloneSetup(personRestController);
+    Person pr = new Person(1L, "Jack", "Sparrow");
+
+    Mockito.when(personService.findPersonById(1L))
+        .thenReturn(pr);
+    System.out.println("Returned person object: " + pr.getName() + " " + pr.getSurname() + " /w ID -> " + pr.getId());
+  }
+
+}
