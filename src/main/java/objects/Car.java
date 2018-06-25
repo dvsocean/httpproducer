78f63866d5ac6993c806a071f9c6aa76543b5d7a
Ocean <dvsocean@icloud.com>
2018-06-25 08:41:15 -0700

    added cars service and controller

--- src/main/java/objects/Car.java
+++ src/main/java/objects/Car.java
@@ -6,10 +6,23 @@ public class Car {
   private String make;
   private String model;
 
+  public Long getId() {
+    return id;
+  }
+
+  public String getMake() {
+    return make;
+  }
+
+  public String getModel() {
+    return model;
+  }
+
   public Car(Long id, String make, String model) {
     this.id = id;
     this.make = make;
     this.model = model;
+
   }
 
 }
---
+++ src/main/java/services/CarService.java
@@ -0,0 +1,25 @@
+package hello;
+
+import objects.Car;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CarService {
+
+  private final Map<Long, Car> carMap;
+
+  public CarService() {
+    carMap = new HashMap<>();
+    carMap.put(1L, new Car(1L, "Ferrai", "Modena"));
+    carMap.put(2L, new Car(2L, "Honda", "Civic"));
+    carMap.put(3L, new Car(3L, "Toyota", "4Runner"));
+    carMap.put(4L, new Car(4L, "Ford", "Raptor"));
+    carMap.put(5L, new Car(5L, "Dodge", "Ram"));
+  }
+
+  Car carById(Long id) {
+    return carMap.get(id);
+  }
+}
